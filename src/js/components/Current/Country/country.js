98652a1670dbfda5bd4ebeb942c3254240b82ea2
Ziwei Lan <ziweilan92@gmail.com>
2020-08-13 13:32:25 +1000

    add styles on country and city input add toggling on country input refactoring

--- src/js/components/Current/Country/country.js
+++ src/js/components/Current/Country/country.js
@@ -13,6 +13,14 @@ class Country extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleBlur = this.handleBlur.bind(this);
         this.handleFocus = this.handleFocus.bind(this);
+        this.toggleInput = this.toggleInput.bind(this);
+    }
+
+    toggleInput() {
+        this.setState({
+            hideInput : !this.state.hideInput,
+            countryValue: this.defaultValue
+        });
     }
 
     updateDisplay(inputValue) {
@@ -28,6 +36,7 @@ class Country extends Component {
     handleSubmit(e) {
         e.preventDefault();
         this.props.onCountryChange(this.state.countryValue);
+        this.toggleInput();
     }
 
     handleFocus(e) {
@@ -46,18 +55,24 @@ class Country extends Component {
         const {countryValue, hideInput} = this.state;
         return (
             <div className = 'Country'>
-                <div className = 'Country__name'>
+                <div    className = 'Country__name'
+                        onClick = {this.toggleInput}
+                >
                     {this.props.country}
                 </div>
-                <form   className = 'Country__form'
-                        onSubmit = {this.handleSubmit}
-                >
-                    <input  value = {countryValue}
-                            onChange = {this.handleChange}
-                            onBlur = {this.handleBlur}
-                            onFocus = {this.handleFocus}
-                    />
-                </form>    
+                {hideInput?
+                    ''
+                :
+                    <form   className = 'Country__form'
+                            onSubmit = {this.handleSubmit}
+                    >
+                        <input  value = {countryValue}
+                                onChange = {this.handleChange}
+                                onBlur = {this.handleBlur}
+                                onFocus = {this.handleFocus}
+                        />
+                    </form> 
+                }   
             </div>
         );
     }
---
+++ src/js/components/Weather/Weather.js
@@ -0,0 +1,165 @@
+import React, {Component} from 'react';
+import Current from '../Current/Current';
+import WeatherBottom from '../WeatherBottom/WeatherBottom';
+import getWeather from '../../api/getWeather';
+import {getCode, getName} from 'country-list';
+
+/* data format									
+data
+	- cityName
+	- current
+		temperature	
+		humidity
+		wind
+		weather
+		icon
+		description
+	- forecast[0]-[4] (same a current)
+	- countryCode 
+*/
+
+class Weather extends Component {
+	constructor (props) {
+		super(props);
+		this.state ={
+			countryCode : 'AU', 
+			city : 'Melbourne',
+			data : null,
+			loading : true,
+			otherCities: []
+		};
+		this.onCityChange = this.onCityChange.bind(this);
+		this.onCountryChange = this.onCountryChange.bind(this);
+		this.onOtherCitiesClick = this.onOtherCitiesClick.bind(this);
+	}
+
+
+	checkCityInput(inputCity) {
+		const index = this.state.otherCities.findIndex((city) => {
+			return city.cityName.toUpperCase() === inputCity.toUpperCase();
+		});
+		if(index > 0){
+			this.onOtherCitiesClick(this.state.otherCities[index].cityName);
+			console.log('click');
+			return false;
+		}else{
+			return (inputCity.toUpperCase() !== this.state.city.toUpperCase());
+		}
+	}
+
+	async updateCity(inputCity) {
+		this.setState({
+			loading : true
+		});
+		const newData = await getWeather(this.state.countryCode, inputCity);
+		if(newData === undefined) {
+			this.setState({
+				loading : false
+			});
+			return alert('country or city can not found');
+		}
+		const newCity = newData.cityName;
+		const newOtherCities = this.state.otherCities.map((city) => city);
+		newOtherCities.shift();
+		newOtherCities.push(this.state.data);
+		console.log('update');
+		this.setState({
+			city: newCity,
+			data: newData,
+			otherCities : newOtherCities,
+			loading : false
+		});
+	}
+
+	onCityChange(inputCity) {
+		return this.checkCityInput(inputCity) && this.updateCity(inputCity);
+	}
+
+	getCountryCode(inputCountry) {
+		const countryCodeLength = 2;
+		const validatedInput = getCode(inputCountry) || getName(inputCountry);
+		if(validatedInput){
+			return (validatedInput.length > countryCodeLength)? inputCountry : validatedInput; 
+		}
+		return false;
+	}
+
+	onCountryChange(inputCountry){
+		const countryCode = this.getCountryCode(inputCountry);
+		console.log(countryCode);
+		if(countryCode) {
+			this.setState({
+				countryCode: countryCode.toUpperCase()
+			});
+		} 	
+	}
+
+	onOtherCitiesClick(selectedCity) {
+		const newOtherCities = this.state.otherCities.map((city) => city);
+		const selectedIndex = newOtherCities.findIndex((city) => city.cityName === selectedCity);
+		const newData = newOtherCities.splice(selectedIndex, 1, this.state.data)[0];
+
+		this.setState({
+			data : newData,
+			otherCities : newOtherCities
+		});
+	}
+
+	async initialRequest () {
+		this.setState({
+			loading : true
+		});
+		const {countryCode: country, city} = this.state;
+		const currentData = await getWeather(country, city);
+		const otherCityNameList = ['Sydney', 'Brisbane', 'Perth'];
+		const otherCityList = [];
+		for(let i = 0; i < otherCityNameList.length; i++) {
+			let city = otherCityNameList[i];
+			let otherCityData = await getWeather(country, city);
+			otherCityList.push(otherCityData);
+		}
+		this.setState({
+			data : currentData,
+			loading : false,
+			otherCities : otherCityList
+		});
+	}
+
+	componentDidMount() {
+		this.initialRequest();
+	}
+
+	render () {
+		const loadingStyle = {
+			borderRadius : '32px',
+		};
+		const {countryCode, data, loading, otherCities} = this.state;
+	
+		return (
+			<div className = 'Weather'>
+				{loading?
+					<div className = 'Current' style = {loadingStyle}>
+						<div className = 'loading'>
+							Loading...
+						</div>
+					</div>
+				:
+					<div>
+						<Current 	current = {data.current}
+									city = {data.cityName}
+									country = {countryCode}
+									onCityChange = {this.onCityChange}
+									onCountryChange = {this.onCountryChange}
+						/>
+						<WeatherBottom 	cityArray = {otherCities}
+										forecastArray = {data.forecast}
+										onOtherCitiesClick = {this.onOtherCitiesClick}
+						/>
+					</div>
+				}
+			</div>
+		);
+	}
+}
+
+export default Weather;
