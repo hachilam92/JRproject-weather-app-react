9b65d51c359e67572cbb080a9f30d67a1e319dec
Ziwei Lan <ziweilan92@gmail.com>
2020-08-17 20:07:58 +1000

    move 'countryCode' state to 'current' component

--- src/js/components/Current/current.js
+++ src/js/components/Current/current.js
@@ -1,30 +1,71 @@
 import React from 'react';
+import {Component} from 'react';
 import './current.scss';
+import {getCode, getName} from 'country-list';
 import Country from './Country/country';
 import CurrentCity from './City/city';
 import CurrentInfo from './Info/info';
 
-function Current (props) {
-	return (
-		<div className = 'Current'>
-			<Country 	country = {props.country}
-						onCountryChange = {props.onCountryChange}
-			/>
-			<CurrentInfo    temperature = {props.current.temperature}
-							weather = {props.current.weather}
-							humidity = {props.current.humidity}
-							wind = {props.current.wind}
-			/>
-			<CurrentCity 	city = {props.city}
-							country = {props.country}
-							checkCityInput = {props.checkCityInput}
-							toggleLoading = {props.toggleLoading}
-							updateDataArray ={props.updateDataArray}
-							onCityChange = {props.onCityChange}
-			/>
-			<div className = 'CurrentBottom'></div>
-		</div>
-	);
+
+class Current extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			countryCode: this.props.country
+		};
+		this.onCountryChange = this.onCountryChange.bind(this);
+	}
+
+	updateCountryCode(newCode) {
+		this.setState({
+			countryCode: newCode
+		});
+	}
+	
+	componentDidUpdate(prevProps) {
+		if(this.props.country !== prevProps.country) {
+			this.updateCountryCode(this.props.country);
+		}
+	}
+
+	getCountryCode(inputCountry) {
+		const countryCodeLength = 2;
+		const validatedInput = getCode(inputCountry) || getName(inputCountry);
+		if(validatedInput){
+			return (validatedInput.length > countryCodeLength)? inputCountry : validatedInput; 
+		}
+		return false;
+	}
+
+	onCountryChange(inputCountry){
+		const countryCode = this.getCountryCode(inputCountry);
+		if(countryCode) {
+			this.updateCountryCode(countryCode.toUpperCase());
+		} 	
+	}
+	
+	render() {
+		return (
+			<div className = 'Current'>
+				<Country 	country = {this.state.countryCode}
+							onCountryChange = {this.onCountryChange}
+				/>
+				<CurrentInfo    temperature = {this.props.current.temperature}
+								weather = {this.props.current.weather}
+								humidity = {this.props.current.humidity}
+								wind = {this.props.current.wind}
+				/>
+				<CurrentCity 	city = {this.props.city}
+								country = {this.state.countryCode}
+								checkCityInput = {this.props.checkCityInput}
+								toggleLoading = {this.props.toggleLoading}
+								updateDataArray ={this.props.updateDataArray}
+								onCityChange = {this.props.onCityChange}
+				/>
+				<div className = 'CurrentBottom'></div>
+			</div>
+		);
+	}
 }
 
 export default Current;
--- src/js/components/Weather/Weather.js
+++ src/js/components/Weather/Weather.js
@@ -3,7 +3,6 @@ import './weather.scss';
 import Current from '../Current/current';
 import WeatherBottom from '../WeatherBottom/weather-bottom';
 import getWeather from '../../api/get-weather';
-import {getCode, getName} from 'country-list';
 
 /* data format									
 data
@@ -23,12 +22,11 @@ class Weather extends Component {
 	constructor (props) {
 		super(props);
 		this.state ={
-			countryCode : 'AU', 
 			loading : true,
 			dataArray: []
 		};
+		this.defaultCountry = 'AU';
 		this.defaultCity = ['Melbourne', 'Sydney', 'Brisbane', 'Perth'];
-		this.onCountryChange = this.onCountryChange.bind(this);
 		this.onOtherCitiesClick = this.onOtherCitiesClick.bind(this);
 		this.toggleLoading = this.toggleLoading.bind(this);
 		this.checkCityInput =this.checkCityInput.bind(this);
@@ -69,38 +67,18 @@ class Weather extends Component {
 		this.setDataArray(newDataArray);
 	}
 
-	getCountryCode(inputCountry) {
-		const countryCodeLength = 2;
-		const validatedInput = getCode(inputCountry) || getName(inputCountry);
-		if(validatedInput){
-			return (validatedInput.length > countryCodeLength)? inputCountry : validatedInput; 
-		}
-		return false;
-	}
-
-	onCountryChange(inputCountry){
-		const countryCode = this.getCountryCode(inputCountry);
-		if(countryCode) {
-			this.setState({
-				countryCode: countryCode.toUpperCase()
-			});
-		} 	
-	}
-
 	onOtherCitiesClick(index) {
 		const dataArrayIndex = index + 1;
 		const newDataArray = this.state.dataArray.map((data) => data);
 		const currentData = newDataArray.splice(dataArrayIndex, 1)[0];
 		newDataArray.unshift(currentData);
 		this.setDataArray(newDataArray);
-		this.setState({
-			countryCode : currentData.countryCode,
-		});
 	}
 
-	async initialRequest (cityNameList = this.defaultCity) {
+	async initialRequest () {
 		this.toggleLoading(true);
-		const {countryCode} = this.state;
+		const cityNameList = this.defaultCity;
+		const countryCode = this.defaultCountry;
 		const dataArray = [];
 		for(let i = 0; i < cityNameList.length; i++) {
 			let city = cityNameList[i];
@@ -119,7 +97,7 @@ class Weather extends Component {
 		const loadingStyle = {
 			borderRadius : '32px',
 		};
-		const {countryCode, loading, dataArray} = this.state;
+		const {loading, dataArray} = this.state;
 		const currentCityIndex = 0;
 	
 		return (
@@ -134,7 +112,7 @@ class Weather extends Component {
 					<div>
 						<Current 	current = {dataArray[currentCityIndex].current}
 									city = {dataArray[currentCityIndex].cityName}
-									country = {countryCode}
+									country = {dataArray[currentCityIndex].countryCode}
 									checkCityInput = {this.checkCityInput}
 									toggleLoading = {this.toggleLoading}
 									updateDataArray = {this.updateDataArray}
