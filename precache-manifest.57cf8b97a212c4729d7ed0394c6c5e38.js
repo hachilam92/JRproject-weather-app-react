4cdfd5fb3566452b7768522844a89d5494f8a735
Ziwei Lan <ziweilan92@gmail.com>
2020-08-18 12:56:22 +1000

    Updates

---
+++ precache-manifest.57cf8b97a212c4729d7ed0394c6c5e38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf62c366e718ddf6ecd45a30d3a6f21a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c1c60fc9a2f3ea1010e",
+    "url": "./static/css/main.0af4ee92.chunk.css"
+  },
+  {
+    "revision": "0acc56c2fbf954daebdf",
+    "url": "./static/js/2.9dcf8d18.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "./static/js/2.9dcf8d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c1c60fc9a2f3ea1010e",
+    "url": "./static/js/main.b84a09e9.chunk.js"
+  },
+  {
+    "revision": "a943224081a43dfb8819",
+    "url": "./static/js/runtime-main.5cbae6e3.js"
+  }
+]);
